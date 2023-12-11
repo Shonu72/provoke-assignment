210a867ab0e298342e96fd6bf07bd668207ac055
Shonu72 <shourya7250@gmail.com>
2023-12-11 10:24:40 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import VideoPlayer from './video';
+import './App.css';
 
 function App() {
   // Use an unlisted video ID for testing
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <h1>YouTube Video Player</h1>
+      <p>Assignment by Sonu Kumar </p>
       <VideoPlayer videoId={unlistedVideoId} />
     </div>
   );
